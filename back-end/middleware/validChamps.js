78f4f7e04c95f6c508f257dc1c8633f0364ffb51
Sylvain Yvonneau <yvonneau.sylvain@gmail.com>
2021-10-28 11:51:50 +0200

    protection SQL

---
+++ back-end/middleware/validChamps.js
@@ -0,0 +1,9 @@
+
+module.exports = (req, res, next) => {
+    const regex = /^[a-zA-Z][ \'\-a-zA-Z0-9]*$/;
+    if (!regex.test(req.body.titre) || !regex.test(req.body.contenu)) {
+        res.status(403).json({message : "Vous ne pouvez pas utiliser de caractères spéciaux pour remplir ce formulaire!"})
+    } else {
+        next();
+    }
+};
--- back-end/routes/post.js
+++ back-end/routes/post.js
@@ -3,15 +3,16 @@ const routeur = express.Router();
 
 const postCtrl = require('../controllers/post');
 const commentCtrl = require('../controllers/comment');
+const validChamps = require('../middleware/validChamps');
 
 routeur.get('/', postCtrl.getAllPosts);
 routeur.get('/:id', postCtrl.getOnePost);
-routeur.post('/', postCtrl.createPost);
-routeur.put('/:id', postCtrl.modifyPost);
+routeur.post('/', validChamps, postCtrl.createPost);
+routeur.put('/:id', validChamps, postCtrl.modifyPost);
 routeur.delete('/:id', postCtrl.deletePost);
 
 routeur.get('/:idpost/comment/', commentCtrl.getComments);
-routeur.post('/:idpost/comment/', commentCtrl.createComments);
+routeur.post('/:idpost/comment/', validChamps, commentCtrl.createComments);
 routeur.delete('/:idpost/comment/:idcom', commentCtrl.deleteComments);
 
 module.exports = routeur;
