e522a41b3330025cc274b83e17a5907ae77ce2d0
Sylvain Yvonneau <yvonneau.sylvain@gmail.com>
2021-10-30 23:34:15 +0200

    Validation avant suppression

--- front-end/src/components/Delete.js
+++ front-end/src/components/Delete.js
@@ -1,30 +1,33 @@
 import axios from "axios";
+import { useState } from 'react';
 
 function Delete ({updateLogin, idPost, idComment, setId, idUsed, admin, idLogged, type}) {
-
+    
+    const [confirm, setConfirm] = useState(false);
+    
     function logout() {
         localStorage.removeItem("token");
         updateLogin(false);
     }
-
+    
     function userDelete() {
         let baseURL = `http://localhost:3001/api/auth/${idUsed}`;
         let token = localStorage.getItem("token");
         axios.delete(baseURL, {
             headers: {
-              Authorization: 'Bearer ' + token
+                Authorization: 'Bearer ' + token
             }
-           })
+        })
         .then(() => logout())
         .catch(() => alert("Vous n'êtes pas autorisé à supprimer ce compte!"));
     }
-
+    
     function messageDelete() {
         let baseURL = `http://localhost:3001/api/post/${idPost}`;
         let token = localStorage.getItem("token");
         axios.delete(baseURL, {
             headers: {
-              Authorization: 'Bearer ' + token
+                Authorization: 'Bearer ' + token
             }
         })
         .then(() => {
@@ -35,15 +38,15 @@ function Delete ({updateLogin, idPost, idComment, setId, idUsed, admin, idLogged
             alert("Problème lors de la suppression du message!");
         });
     }
-
+    
     function commentDelete() {
         let baseURL = `http://localhost:3001/api/post/${idPost}/comment/${idComment}`;
         let token = localStorage.getItem("token");
         axios.delete(baseURL, {
             headers: {
-              Authorization: 'Bearer ' + token
+                Authorization: 'Bearer ' + token
             }
-           })
+        })
         .then(() => {
             setId(0);
             setId(idPost);
@@ -51,27 +54,39 @@ function Delete ({updateLogin, idPost, idComment, setId, idUsed, admin, idLogged
         })
         .catch(() => alert("Vous n'êtes pas autorisé à supprimer cette réponse!"));
     }
-
+    
     if (type === "User") {
         if ((idUsed === idLogged) || (admin === true)) {
-            return (
-                <input type="button" value="Supprimer" className="bouton--delete" onClick={() => userDelete()} />
+            return ( (confirm) ?
+            <div>
+            <p>Êtes-vous sûr ?  <span className="bouton--choix" onClick={() => userDelete()}>Oui</span>  <span className="bouton--choix" onClick={() => setConfirm(false)}>Non</span></p>
+            </div>
+            : 
+            <input type="button" value="Supprimer" className="bouton--delete" onClick={() => setConfirm(true)} />
             )
         } else {
             return null;
         }
     } else if (type === "Message") {
         if ((idUsed === idLogged) || (admin === true)) {
-            return (
-                <p className="text--action" onClick={() => messageDelete()}>Supprimer le post</p>
+            return ((confirm) ?
+            <div>
+            <p>Êtes-vous sûr ?  <span className="bouton--choix" onClick={() => messageDelete()}>Oui</span>  <span className="bouton--choix" onClick={() => setConfirm(false)}>Non</ span></p>
+            </div>
+            : 
+            <p className="text--action" onClick={() => setConfirm(true)}>Supprimer le post</p>
             )
         } else {
             return null;
         }
     } else if (type=== "Comment") {
         if ((idUsed === idLogged) || (admin === true)) {
-            return (
-                <p className="text--action" onClick={() => commentDelete()}>Supprimer la réponse</p>
+            return ((confirm) ?
+            <div>
+                <p>Êtes-vous sûr ?  <span className="bouton--choix" onClick={() => commentDelete()}>Oui</span>  <span className="bouton--choix" onClick={() => setConfirm(false)}>Non</ span></p>
+            </div>
+            :
+            <p className="text--action" onClick={() => setConfirm(true)}>Supprimer la réponse</p>
             )
         } else {
             return null;
